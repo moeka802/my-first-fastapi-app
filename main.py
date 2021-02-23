2392cde48e23e812eda4535af5c9384ff1ffd530
moeka802 <42276835+moeka802@users.noreply.github.com>
2021-02-23 23:46:36 +0900

    パスパラメータの型バリデーション

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI 
 
-# uvicorn main:app --reload
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello world"}
+@app.get("/items/{item_id}")
+async def read_item(item_id: int):
+    return {"item_id": item_id}
